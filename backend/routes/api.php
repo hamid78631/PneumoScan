5f8af55b6d05a0cb03171da6b3ede982728e1694
Hamid <hamidoumamoudou125@icloud.com>
2026-07-21 13:47:35 +0100

    feat : authentification and close endpoints

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,8 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnalysisController ;
+use App\Http\Controllers\AuthController ;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/analyses' , [AnalysisController::class , 'store']);
+Route::post('/login' , [AuthController::class , 'login']);
